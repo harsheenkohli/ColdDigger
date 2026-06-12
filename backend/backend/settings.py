81ff5b7c78550d93405ef79e8737101c37b1da06
harsheenkohli <harsheenkohli2003@gmail.com>
2026-06-12 17:29:35 +0530

    correcting cloudinary file type

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,6 +164,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'RESOURCE_TYPE': 'raw',
 }
 
 if os.environ.get('CLOUDINARY_CLOUD_NAME'):
